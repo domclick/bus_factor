2be6da931da675c6a430887c1d7fa5436264712c
a17380229 <iasemin@domclick.ru>
2020-03-25 23:23:06 +0300

    https://angular.io/tutorial/toh-pt3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainPageComponent } from './pages/main-page/main-page.component';
-import { TournamentListComponent } from './tournament-list/tournament-list.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import {TournamentListComponent} from './tournament-list/tournament-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent,
-    TournamentListComponent
+    TournamentListComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
